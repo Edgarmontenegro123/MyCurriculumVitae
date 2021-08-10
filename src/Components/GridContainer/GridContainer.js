a4f3d56e6db7ca687a4f9963eb7d763b061288ba
Edgar Montenegro <edgarmontenegro321@gmail.com>
2021-08-10 15:32:31 -0300

    Latest Version

--- src/Components/GridContainer/GridContainer.js
+++ src/Components/GridContainer/GridContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
-import Photo from '../../Images/EdgarPhoto.jpeg'
+import Photo from '../../Images/Edgar.jpeg'
 
 const useStyles = makeStyles((theme) => ({
   backUp:{
