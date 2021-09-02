277b177b1ad00b98ca54db17cc77c552d126dc9c
Edgar Montenegro <edgarmontenegro321@gmail.com>
2021-09-01 21:02:45 -0300

    Cards with Material UI fixed

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -61,122 +61,70 @@ export default function ItemList({items}){
 
   return(
     <div /* style = {{display: 'flex'}} */>
-        {items.map(i => <Grid container spacing = {4} className = {classes.gridContainer} justify = 'center'>
-    <Grid item xs = {12} sm = {6} lg = {4}>
-      <Card className = {classes.root}>
-        <CardHeader
-          avatar = {
-            <Avatar aria-label = 'recipe' className = {classes.avatar}>
-              <img src = {i.avatar} 
-                   alt = 'C'
-                   width = '30px'
-                   height = '30px'/>      
-            </Avatar>
-          }
-          title = {i.title}
-          subheader = {i.avatarSubheader}
-
-        />
-        <CardMedia
-        className = {classes.media}
-        image = {i.cardMediaImage}
-        alt = {i.name}
-        title = {i.name}
-        >
-        </CardMedia>
-        <CardContent>
-          <Typography variant = 'body2' color = 'textSecondary' component = 'p'>
-          <strong>{i.cardContentTypography}</strong>
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label = 'share' style = {{marginTop : '-5%'}}>
-            <a className = {classes.link} href = {i.iconButton} target = '_blank' rel = 'noreferrer'><i id = 'icon' className = 'fas fa-play'></i></a>
-          </IconButton>
-          <IconButton
-            id = {i.id}
-            className = {clsx(classes.expand, {
-              [classes.expandOpen]: expanded,
-            })}
-            onClick = {handleExpandClick}
-          > 
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in = {expanded} timeout = 'auto' unmountOnExit>
-          <CardContent>
-            <Typography paragraph>{i.typography}</Typography>
-            <Typography paragraph>
-              {i.typographyParagraph} 
-            </Typography>
-            <Typography paragraph>
-              {i.typographyParagraph2}
-              <ul>
-                <li>
-                {i.li1}
-                </li>
-                <li>
-                {i.li2}
-                </li>
-                <li>
-                {i.li3}
-                </li>
-                <li>
-                {i.li4}
-                </li>
-                <li>
-                {i.li5}
-                </li>
-              </ul>
-            </Typography>
-            <Typography paragraph>
-              <strong>Pruébala <a className = {classes.link} id = 'link' href = {i.link1} target = '_blank' rel = 'noreferrer'>Acá!</a></strong>
-            </Typography>
-            <Typography>
-            <strong>Observa el <a className = {classes.link} id = 'link' href = {i.link2} target = '_blank' rel = 'noreferrer'>Código</a> Acá!</strong>
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
-    </Grid>
-  </Grid>)}
-  {/* {items.map(i =>)}
-  <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>{i.typography}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          {i.typographyParagraph}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>{i.typography}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          {i.typographyParagraph}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-        </AccordionSummary>
-      </Accordion> */}
-      </div>
+      {items.map(i => 
+      <Grid container spacing = {4} className = {classes.gridContainer} justify = 'center'>
+        <Grid item xs = {12} sm = {6} lg = {4}>
+          <Card className = {classes.root}>
+            <CardHeader
+              avatar = {
+                <Avatar aria-label = 'recipe' className = {classes.avatar}>
+                  <img src = {i.avatar} 
+                      alt = 'C'
+                      width = '30px'
+                      height = '30px'/>      
+                </Avatar>
+              }
+              title = {i.title}
+              subheader = {i.avatarSubheader}
+            />
+            <CardMedia
+            className = {classes.media}
+            image = {i.cardMediaImage}
+            alt = {i.name}
+            title = {i.name}
+            >
+            </CardMedia>
+            <CardContent>
+              <Typography variant = 'body2' color = 'textSecondary' component = 'p'>
+              <strong>{i.cardContentTypography}</strong>
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing className = {classes.accordion}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <IconButton aria-label = 'share' style = {{marginTop : '-5%'}}>
+                    <a className = {classes.link} href = {i.iconButton} target = '_blank' rel = 'noreferrer'><i id = 'icon' className = 'fas fa-play'></i></a>
+                  </IconButton>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {i.typographyParagraph}
+                    <br/>
+                    {i.typographyParagraph2}
+                    <ul>
+                      <li>{i.li1}</li>
+                      <li>{i.li2}</li>
+                      <li>{i.li3}</li>
+                      <li>{i.li4}</li>
+                      <li>{i.li5}</li>
+                    </ul>
+                    <Typography paragraph>
+                      <strong>Pruébala <a className = {classes.link} id = 'link' href = {i.link1} target = '_blank' rel = 'noreferrer'>Acá!</a></strong>
+                    </Typography>
+                    <Typography>
+                      <strong>Observa el <a className = {classes.link} id = 'link' href = {i.link2} target = '_blank' rel = 'noreferrer'>Código</a> Acá!</strong>
+                    </Typography>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>)}
+    </div>
   )
 }
