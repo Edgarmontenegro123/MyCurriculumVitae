75496df82b6c2778a5d14dc12d1ff73c1de92d58
Edgar Montenegro <edgarmontenegro321@gmail.com>
2021-09-03 04:19:59 -0300

    Cards Fixed

--- src/Components/MockUp/MockUp.js
+++ src/Components/MockUp/MockUp.js
@@ -7,7 +7,6 @@ const MockUp = [{
     id: 1,
     avatar: Logo, 
     title: 'ClimApp', 
-<<<<<<< HEAD
     date: 'Marzo 2021', 
     img: gifClimApp, 
     resume: 'Primera App FrontEnd realizada con React y una API externa.',
@@ -16,27 +15,10 @@ const MockUp = [{
     tech: 'JavaScript, React, Material UI, HTML5, CSS3',
     deploy: 'https://climappmontenegro.vercel.app',
     gitHub: 'https://github.com/Edgarmontenegro123/ClimApp'},
-=======
-    avatarSubheader: 'Marzo 2021', 
-    cardMediaImage: gifClimApp, 
-    cardContentTypography: 'Primera App FrontEnd realizada con React y una API externa.',
-    iconButton: 'https://vimeo.com/manage/videos/586698718',
-    typography: 'ClimApp: ',
-    typographyParagraph: 'Esta es una App con una interfaz amigable, que muestra la hora y la fecha detallada del lugar donde te encuentres, con un buscador intuitivo, para que elijas tus ciudades favoritas y en ellas puedas observar los datos climáticos más relevantes.',
-    typographyParagraph2: 'Para realizarla se utilizaron tecnologías como: ', 
-    li1: 'JavaScript',
-    li2: 'React',
-    li3: 'Material UI',
-    li4: 'HTML5',
-    li5: 'CSS3',
-    link1: 'https://climappmontenegro.vercel.app',
-    link2: 'https://github.com/Edgarmontenegro123/ClimApp'},
->>>>>>> b75a3afa4e42bdc1bfc99dfc90e4676e3486c8bc
     {
     id: 2,
     avatar: Logo2, 
     title: 'Healthier', 
-<<<<<<< HEAD
     date: 'Agosto 2021', 
     img: gifHealthier, 
     resume: 'E-Commerce realizado de manera grupal como proyecto final para Soy Henry Bootcamp.',
@@ -45,21 +27,5 @@ const MockUp = [{
     tech: 'React, Redux, Material UI, NodeJS, PostgreSQL',
     deploy: 'https://Healthier.vercel.app',
     gitHub: 'https://github.com/Healthier-Group/Healthier'}]
-=======
-    avatarSubheader: 'Agosto 2021', 
-    cardMediaImage: gifHealthier, 
-    cardContentTypography: 'E-Commerce realizado de manera grupal como proyecto final para Soy Henry Bootcamp.',
-    iconButton: 'https://vimeo.com/manage/videos/586633960',
-    typography: 'Healthier: ',
-    typographyParagraph: 'Con una interfaz muy intuitiva, que permite comprar productos saludables, ordenarlos por precio, por categoría, incluirlos en una wishlist  o llevarlos a un carrito de compra donde se permite terminar el proceso de compra mediante una pasarela de pagos.',
-    typographyParagraph2: 'Para realizarla se utilizaron tecnologías como: ', 
-    li1: 'React',
-    li2: 'Redux',
-    li3: 'Material UI',
-    li4: 'NodeJS',
-    li5: 'PostgreSQL',
-    link1: 'https://Healthier.vercel.app',
-    link2: 'https://github.com/Healthier-Group/Healthier'}]
->>>>>>> b75a3afa4e42bdc1bfc99dfc90e4676e3486c8bc
 
     export default MockUp
